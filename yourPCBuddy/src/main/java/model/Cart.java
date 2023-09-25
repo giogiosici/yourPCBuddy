09d8c861737761d4305a7542b5586930b689f212
giogiosici <giovanni.sicilia02gmail.com>
2023-09-26 01:45:31 +0200

    Risoluzione problemi minori causa merge di git

--- yourPCBuddy/src/main/java/model/Cart.java
+++ yourPCBuddy/src/main/java/model/Cart.java
@@ -5,8 +5,22 @@ import java.util.*;
 
 public class Cart {
 	
+
+	
 	double totalPrice=0;
 	private int quantity;
+	private int code;
+	
+	
+	public int getCode() {
+		return code;
+	}
+	
+	public void setCode(int code) {
+		this.code=code;
+	}
+	
+
 
     public int getQuantity() {
         return quantity;
@@ -26,10 +40,22 @@ public class Cart {
             if (prod.getCode() == product.getCode()) {
                 // Il prodotto è già nel carrello, quindi incrementa la quantità
                 prod.setQuantity(prod.getQuantity() + 1);
+
+                totalPrice += product.getPrice();
+                setTotalPrice(totalPrice);
+                return;
+            }
+        
+        // Se il prodotto non è nel carrello, aggiungilo con quantità 1
+       
+        product.setQuantity(1);
+        totalPrice += product.getPrice();
+        setTotalPrice(totalPrice);
+
                 
                 return;
             }
-        }
+        
         // Se il prodotto non è nel carrello, aggiungilo con quantità 1
        
         product.setQuantity(1);
@@ -39,10 +65,14 @@ public class Cart {
 	public void deleteProduct(ProductBean product) {
 		for(ProductBean prod : products) {
 			if(prod.getCode() == product.getCode()) {
+				totalPrice -= product.getPrice();
+				setTotalPrice(totalPrice);
 				if(prod.getQuantity()<=1)
 				products.remove(prod);
 				else
 					prod.setQuantity(prod.getQuantity() - 1);
+					totalPrice -= product.getPrice();
+					setTotalPrice(totalPrice);
 				return;
 			}
 		}
@@ -82,6 +112,10 @@ public class Cart {
 		return totalPrice;
 	}
 	public void addProducts(Collection<ProductBean> product) {
+		for(ProductBean prod : product) {
+			totalPrice += (prod.getPrice()*prod.getQuantity());
+		}
+		setTotalPrice(totalPrice);
 		products.addAll(product);
 	}
 	
--- yourPCBuddy/src/main/java/model/CartDaoDataSource.java
+++ yourPCBuddy/src/main/java/model/CartDaoDataSource.java
@@ -193,6 +193,7 @@ public class CartDaoDataSource  implements CartDao{
 			    bean.setName(rsProducts.getString("Nome"));
 			    bean.setPrice(rsProducts.getFloat("Prezzo"));
 			    bean.setImage(rsProducts.getString("Immagine"));
+
 			    
 			    // Popola il campo quantity
 			    bean.setQuantity(rsProducts.getInt("QuantitaProdotto"));
