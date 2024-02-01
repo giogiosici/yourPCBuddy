ef4a62ce51ee75f7aa48995e4afa8b02bb0586b4
giogiosici <127218555+giogiosici@users.noreply.github.com>
2024-02-01 22:28:33 +0100

    Add files via upload

---
+++ yourPCBuddy/src/main/WebContent/AdminPage.jsp
@@ -0,0 +1,33 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+    <%@ page import="model.User" %>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Admin Page</title>
+</head>
+<%
+User user = (User) session.getAttribute("user");
+int flag = 0;
+if (user == null)
+	response.sendRedirect("LoginScreen.jsp");
+else if (user.getId() == 1) {
+	flag = 1;
+}
+%>
+<body>
+
+<form action="LogoutServlet" method="POST">
+        <input type="submit" value="Logout">
+    </form>
+    
+<form action="ProductControl" method="post">
+		<input type="submit" value="Prodotti">
+	</form>
+	
+	<form action="AdminOrderView.jsp" method="post">
+		<input type="submit" value="Ordini">
+	</form>
+</body>
+</html>
---
+++ yourPCBuddy/src/main/WebContent/Scripts/PaginaProdottoScript.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+	
+	$('.DetailsProduct').submit(function(e) {
+		e.preventDefault(); // Previene l'invio predefinito del form
+console.log("a puttan ra mamm");
+		var formData = $(this).serialize(); // Ottiene i dati del form serializzati
+
+		// Trova il campo "quantity" associato a questo form
+		var quantityField = $('#quantity_' + $(this).find('input[name="id"]').val());
+
+		// Ottieni il valore corrente del campo "quantity" e convertilo in un numero intero
+		var currentQuantity = parseInt(quantityField.val());
+
+		// Incrementa il valore di "quantity" di 1
+		var newQuantity = currentQuantity + 1;
+
+		// Imposta il nuovo valore di "quantity" nel campo
+		quantityField.val(newQuantity);
+		console.log("prima di ajax");
+		// Invia la richiesta AJAX al tuo form di destinazione
+		$.ajax({
+			url: 'CartServlet',
+			type: 'POST',
+			data: formData,
+			success: function(response) {
+				// Gestisci la risposta del server, se necessario
+			},
+			error: function(xhr, status, error) {
+				// Gestisci gli errori, se necessario
+			}
+		});
+	});
+});
